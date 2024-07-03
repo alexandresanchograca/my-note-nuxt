02576580e27f30c257a2f599909d94e7a1e8e2b4
alexandresanchograca <alexandresanchograca@gmail.com>
2024-07-03 22:38:38 +0100

    Uncommented the authentication watcher handler.

--- app.vue
+++ app.vue
@@ -11,24 +11,21 @@
 const router = useRouter()
 const route = useRoute()
 const nuxtApp = useNuxtApp()
+const user = useCurrentUser()
 
-// console.log(nuxtApp)
-//const user = useCurrentUser()
-
-// console.log("userCurrentUser", user.value);
 
 // Checking if user is authed
 // OnMounted hooks only get called on client
-// onMounted(() => {
-//   watch(user, (user, prevUser) => {
-//     // console.log("Watch user", user);
-//     if (prevUser && !user) {
-//       // user logged out
-//       router.push('/login')
-//     } else if (user && typeof route.query.redirect === 'string') {
-//       // user logged in
-//       router.push(route.query.redirect)
-//     }
-//   })
-// })
+onMounted(() => {
+  watch(user, (user, prevUser) => {
+    // console.log("Watch user", user);
+    if (prevUser && !user) {
+      // user logged out
+      router.push('/login')
+    } else if (user && typeof route.query.redirect === 'string') {
+      // user logged in
+      router.push(route.query.redirect)
+    }
+  })
+})
 </script>
